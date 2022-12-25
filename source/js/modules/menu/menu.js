965333a2578e1d010dfcc9b8b5429fea60ff7be0
AlexDrags <vera2052@mail.ru>
2022-12-25 23:14:09 +0300

    большой фикс по работе меню

--- source/js/modules/menu/menu.js
+++ source/js/modules/menu/menu.js
@@ -1,15 +1,25 @@
 const menuList = document.querySelector('.menu-js');
 const buttonMenu = document.querySelector('.button-js');
 const body = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.main-nav__link');
+
+function closeMenu() {
+  body.classList.toggle('overlay');
+  menuList.classList.toggle('main-nav__list--nojs--open');
+}
 
 function openMenu() {
   body.classList.toggle('overlay');
   menuList.classList.toggle('main-nav__list--nojs--open');
   buttonMenu.classList.toggle('button-menu__icon--close');
 }
+
 export default function initNoJs() {
   if (menuList.classList.contains('main-nav__list--nojs--open')) {
     menuList.classList.remove('main-nav__list--nojs--open');
   }
   buttonMenu.addEventListener('click', openMenu);
+  for (let menuLink of menuLinks) {
+    menuLink.addEventListener('click', closeMenu);
+  }
 }
