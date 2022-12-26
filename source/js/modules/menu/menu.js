47dd41ff627ac46fd5b6500d082c8ab3dc98c69a
AlexDrags <vera2052@mail.ru>
2022-12-26 13:32:17 +0300

    решение бага с появлением скролла

--- source/js/modules/menu/menu.js
+++ source/js/modules/menu/menu.js
@@ -3,9 +3,13 @@ const buttonMenu = document.querySelector('.button-js');
 const body = document.querySelector('body');
 const menuLinks = document.querySelectorAll('.main-nav__link');
 
+
 function closeMenu() {
-  body.classList.toggle('overlay');
-  menuList.classList.remove('main-nav__list--nojs--open');
+  if (body.classList.contains('overlay') && menuList.classList.contains('main-nav__list--nojs--open')) {
+    menuList.classList.remove('main-nav__list--nojs--open');
+    body.classList.remove('overlay');
+    buttonMenu.classList.toggle('button-menu__icon--close');
+  }
 }
 
 function openMenu() {
@@ -17,9 +21,15 @@ function openMenu() {
 export default function initNoJs() {
   if (menuList.classList.contains('main-nav__list--nojs--open')) {
     menuList.classList.remove('main-nav__list--nojs--open');
-  }
-  buttonMenu.addEventListener('click', openMenu);
-  for (let menuLink of menuLinks) {
-    menuLink.addEventListener('click', closeMenu);
+    buttonMenu.addEventListener('click', openMenu);
+    for (let menuLink of menuLinks) {
+      menuLink.addEventListener('click', closeMenu);
+    }
+    document.addEventListener('click', (e)=>{
+      let target = e.target;
+      if (target !== menuList && target !== buttonMenu && menuList.classList.contains('main-nav__list--nojs--open')) {
+        closeMenu();
+      }
+    });
   }
 }
